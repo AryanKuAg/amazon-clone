4772840331eebda37ac433bdfdb2cc8d1d11ead3
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-03-10 23:58:01 +0530

    material icons added

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
 const Header = () => {
   return (
     <div className="header">
@@ -7,6 +8,24 @@ const Header = () => {
         className="header__logo"
         src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
       />
+      <div className="header__search">
+        <input type="text" className="header__searchInput"></input>
+        <SearchIcon />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello</span>
+          <span className="header__optionLineTwo">User</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello</span>
+          <span className="header__optionLineTwo">User</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello</span>
+          <span className="header__optionLineTwo">User</span>
+        </div>
+      </div>
     </div>
   );
 };
