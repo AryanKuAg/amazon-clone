6fa72429eb96d3be027c4e6d66134e8cb26994c2
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-03-11 01:27:59 +0530

    nice backgrond added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       {/* Header */}
+      <Home />
       {/* body */}
       {/* footer */}
     </div>
---
+++ src/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          className="home__image"
+        />
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">
+          {/* Product */}
+          {/* Product */}
+          {/* Product */}
+        </div>
+        <div className="home__row">{/* Product */}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
