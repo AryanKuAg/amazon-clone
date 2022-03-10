b250f4ca22fdfacd795d7a08e6d31ebecc511044
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-03-10 23:09:48 +0530

    pngimage.com is so cool for png images

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="https://pngimg.com/uploads/amazon/amazon_PNG25.png"
+      />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAkxC8mG4Wd1hO0HNUcG0GN6Od1ahPgov0",
+  authDomain: "clone-5b4a8.firebaseapp.com",
+  projectId: "clone-5b4a8",
+  storageBucket: "clone-5b4a8.appspot.com",
+  messagingSenderId: "1022670865636",
+  appId: "1:1022670865636:web:91f2804682b3fd0e7998f1",
+  measurementId: "G-LYDEF8YMJH",
+};
