f8ea83b0352bb1323ca02eb443374eb80beae5f3
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-03-11 01:00:38 +0530

    header completed day 2 started

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 const Header = () => {
   return (
     <div className="header">
@@ -10,21 +11,26 @@ const Header = () => {
       />
       <div className="header__search">
         <input type="text" className="header__searchInput"></input>
-        <SearchIcon />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineTwo">User</span>
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign In</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineTwo">User</span>
+          <span className="header__optionLineOne">Returns {"&"} </span>
+          <span className="header__optionLineTwo">Orders</span>
         </div>
         <div className="header__option">
-          <span className="header__optionLineOne">Hello</span>
-          <span className="header__optionLineTwo">User</span>
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
         </div>
+      </div>
+
+      <div className="header__optionBasket">
+        <ShoppingBasketIcon />
+        <span className="header__basketCount header__optionLineTwo">0</span>
       </div>
     </div>
   );
